2de6ec494775dc2224c366ed793e54a2f93c712d
allenabraham777 <allenabraham777@gmail.com>
2024-02-11 19:55:50 +0530

    feat(ds): add binary tree node

---
+++ data-structures/tree/src/node/binary-tree-node.ts
@@ -0,0 +1,44 @@
+export class BinaryTreeNode<T> {
+    private _value: T;
+    private _left: BinaryTreeNode<T> | null;
+    private _right: BinaryTreeNode<T> | null;
+    constructor(value: T) {
+        this._value = value;
+        this._left = null;
+        this._right = null;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    get left() {
+        return this._left;
+    }
+
+    set setLeft(_left: BinaryTreeNode<T> | null) {
+        this._left = _left;
+    }
+
+    get right() {
+        return this._right;
+    }
+
+    set setRight(_right: BinaryTreeNode<T> | null) {
+        this._right = _right;
+    }
+
+    get leftHeight(): number {
+        if (!this.left) return 0;
+        return this.left.height + 1;
+    }
+
+    get rightHeight(): number {
+        if (!this.right) return 0;
+        return this.right.height + 1;
+    }
+
+    get height(): number {
+        return Math.max(this.leftHeight, this.rightHeight);
+    }
+}
